4cbfc4e5a85988caa8f1eaa9a7bda334e9068698
Riccardo Balbo <riccardo0blb@gmail.com>
2024-04-26 19:23:18 +0200

    change test relay

--- relays.js
+++ relays.js
@@ -1,4 +1,6 @@
 window.nostrRelays=[
+    "wss://oa.forkforge.net:7777"
     // "wss://nostr.rblb.it:7777"
-    "wss://openagents.forkforge.net:7777"
+
+    // "wss://openagents.forkforge.net:7777"
 ];
