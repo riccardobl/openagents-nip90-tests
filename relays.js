b11c3d7db0ac829ef5bfd1c1288275981b0198fa
Riccardo Balbo <riccardo0blb@gmail.com>
2024-04-29 22:41:51 +0200

    Update tests

--- relays.js
+++ relays.js
@@ -1,6 +1,6 @@
 window.nostrRelays=[
-    "wss://oa.forkforge.net:7777"
+    // "wss://oa.forkforge.net:7777"
     // "wss://nostr.rblb.it:7777"
-
+    "wss://nostr.openagents.com:7777"
     // "wss://openagents.forkforge.net:7777"
 ];
