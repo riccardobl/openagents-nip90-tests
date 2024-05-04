c08f5575f81ac107b25e3e377b3d2e3b3194698b
Riccardo Balbo <riccardo0blb@gmail.com>
2024-05-04 13:18:21 +0200

    Add staging relay, update tests

--- relays.js
+++ relays.js
@@ -1,6 +1,4 @@
 window.nostrRelays=[
-    // "wss://oa.forkforge.net:7777"
-    // "wss://nostr.rblb.it:7777"
-    "wss://nostr.openagents.com:7777"
-    // "wss://openagents.forkforge.net:7777"
+    "wss://oast0.forkforge.net:7777",
+    "wss://nostr.openagents.com:7777"    
 ];
